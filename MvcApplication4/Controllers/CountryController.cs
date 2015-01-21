b0c6ee67a442654a900489c3f2fdf8194851e6a6
unknown <in8mohka@DKV1679.corp.lego.com>
2015-01-21 20:47:02 +0530

    first comit

---
+++ MvcApplication4/Controllers/CountryController.cs
@@ -0,0 +1,42 @@
+﻿using MvcApplication4.Models;
+using System.Linq;
+using System.Web.Mvc;  
+namespace Employee.Controllers 
+{ 
+    public class CountryController : Controller 
+    { 
+        public ActionResult Index() 
+        { 
+            return View(); 
+        }
+
+        public ActionResult details(LoginDetails loginInfo) 
+        {
+            LoginDetails da = new LoginDetails();
+            string[] arrUser = new string[] { "ankit", "vipin", "ashumam", "anujsir", "hello", "kashif" }; 
+            string[] arrPass = new string[] { "12345", "6789", "1234", "5678", "word", "123456" }; 
+            da.UserName = loginInfo.UserName; 
+            da.Password = loginInfo.Password;
+            if (da.UserName == null || da.Password == null || da.UserName.Equals(da.Password))
+            { 
+                ViewBag.message = "Fill the information"; 
+                return View("Index", da); 
+            } 
+          if (arrUser.Contains(da.UserName) && arrPass.Contains(da.Password)) 
+          { 
+              Session["user"] = da.UserName; 
+                return RedirectToAction("success"); 
+            } 
+            else 
+            { 
+                ViewBag.message = "Invalid Username or Passowrd"; 
+                return View("Index", da); 
+            } 
+        } 
+        public ActionResult success() 
+        { 
+           ViewBag.name = Session["user"].ToString(); 
+            return View("Success"); 
+        } 
+    } 
+}
---
+++ MvcApplication4/Models/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication4.Models
+{
+    public class Country
+    {
+        public string CountryName { get; set;}
+        public string CountryCode { get; set;}
+    }
+    public class LoginDetails
+    {
+        [Required(ErrorMessage = "Enter the UserName")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Enter the Password")]
+        public string Password { get; set; }
+        
+    }
+
+   
+    
+}
+
+
